977115472cbc8089ec932ae368abf63d23d8fcf6
abvgmbh <gaertner@abv-projekt.com>
2021-03-12 11:35:43 +0100

    async await implementation

---
+++ src/server/routes/weather.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const fetch = require("node-fetch");
+
+const router = express();
+
+function getGeo(city) {
+    const geoURL = `http://api.geonames.org/searchJSON?q=${city}&maxRows=1&username=kamara.moses`;
+    return fetch(geoURL).then(res => res.json());
+}
+
+function getWeather(lat, lng) {
+    const weatherBitURL = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lng}&key=cda6df51d9a24b8c9d54b830f4eadb51`;
+    return fetch(weatherBitURL).then(res => res.json());
+}
+
+function getPixaImage(city) {
+    const pixaBayURL = `https://pixabay.com/api/?key=19853981-85155ca595da994be43f034e6&q=${city}&image_type=photo`;
+    return fetch(pixaBayURL).then(res => res.json());
+}
+
+router.post("/getWeather", async (req, res) => {
+    const { city } = req.body;
+
+    const reply = {
+        city,
+        icon: null,
+        description: "",
+        highTemp: 0,
+        lowTemp: 0,
+        image: "",
+        errors: {}
+    };
+
+    let lat, lng;
+
+    // Call to the geonames API
+    try {
+        const geoData = await getGeo(city);
+        console.log(geoData);
+        lat = geoData.geonames[0].lat;
+        lng = geoData.geonames[0].lng;
+    }
+    catch (e) {
+        errors.geoname = e.message;
+    }
+
+    // call to WeatherBit API
+    if (lat && lng) {
+        try {
+            const weatherData = await getWeather(lat, lng);
+            console.log(weatherData.data[0]);
+            const { weather: { icon, description }, high_temp: highTemp, low_temp: lowTemp } = weatherData.data[0];
+            reply.icon = icon;
+            reply.description = description;
+            reply.highTemp = highTemp;
+            reply.lowTemp = lowTemp;
+        }
+        catch (e) {
+            errors.weatherBit = e.message;
+        }
+    }
+
+    // call to PixaBay API
+    try {
+        const pixaData = await getPixaImage(city);
+        console.log(pixaData.hits[0]);
+        reply.image = pixaData.hits[0].webformatURL;
+    }
+    catch (e) {
+        errors.pixaBay = e.message;
+    }
+
+    console.log(reply);
+    res.send(reply);
+});
+
+module.exports = router;
--- src/server/server.js
+++ src/server/server.js
@@ -10,7 +10,6 @@ const app = express();
 /* Dependencies */
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const fetch = require("node-fetch");
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -23,60 +22,12 @@ app.use(cors());
 // Initialize the main project folder
 app.use(express.static("dist"));
 
+// routes
+const weather = require('./routes/weather');
+app.use(weather);
+
 // Setup Server
 const port = 8000;
-const server = app.listen(port, listening);
-
-function listening() {
+app.listen(port, function () {
   console.log(`Server is running on port ${port}`);
-}
-
-// Asked a question on stackoverflow in regards to chaining.
-// I was giving an example of how to make chaining api calls
-// One user suggested moving the api call from the client side to the server side.
-app.post("/getWeather", projectData = (req, res) => {
-  const { city } = req.body;
-  // Call to the geonames API
-  const geoNames = `http://api.geonames.org/searchJSON?q=${city}&maxRows=1&username=kamara.moses`;
-  fetch(geoNames)
-    .then((res) => res.json())
-    .then((json) => {
-      // getting latitude and longitude
-      const lat = json.geonames[0].lat;
-      const lng = json.geonames[0].lng;
-      console.log(geoNames);
-      console.log(lat);
-      console.log(lng);
-
-      // Call to the weatherbit API
-      const weatherBit = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lng}&key=cda6df51d9a24b8c9d54b830f4eadb51`;
-      fetch(weatherBit)
-        .then((res) => res.json())
-        .then((json) => {
-          console.log(json);
-          const icon = json.data[0].weather.icon;
-          const description = json.data[0].weather.description;
-          const highTemp = json.data[0].high_temp;
-          const lowTemp = json.data[0].low_temp;
-
-          // Call to the pixabay API
-          const pixaBay = `https://pixabay.com/api/?key=19853981-85155ca595da994be43f034e6&q=${city}&image_type=photo`;
-          fetch(pixaBay)
-            .then((res) => res.json())
-            .then((json) => {
-              console.log(json);
-              const image = json.hits[0].webformatURL;
-              const pixObj = {
-                city: city,
-                icon: icon,
-                description: description,
-                highTemp: highTemp,
-                lowTemp: lowTemp,
-                image: image,
-              };
-              res.send(pixObj);
-              console.log(pixObj);
-            });
-        });
-    });
 });
